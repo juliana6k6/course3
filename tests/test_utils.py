699eeb9eebe92958a2d912011c1338b6699615e3
petrova_juliana <juliana6k6@gmail.com>
2023-10-10 22:32:54 +0300

    func utils tests дораб1

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -106,109 +106,39 @@ def test_filter_operations():
     "from": "Visa Gold 5999414228426353",
     "to": "Счет 72731966109147704472"
   },
-  {
-    "id": 863064926,
-    "state": "EXECUTED",
-    "date": "2019-12-08T22:46:21.935582",
-    "operationAmount": {
-      "amount": "41096.24",
-      "currency": {
-        "name": "USD",
-        "code": "USD"
-      }
-    },
-    "description": "Открытие вклада",
-    "to": "Счет 90424923579946435907"
-  }]
+  ]
 
 def test_sort_operations_five():
   assert sort_operations_five([
   {
     "id": 441945886,
     "state": "EXECUTED",
-    "date": "2019-08-26T10:50:58.294041",
-    "operationAmount": {
-      "amount": "31957.58",
-      "currency": {
-        "name": "руб.",
-        "code": "RUB"
-      }
-    },
-    "description": "Перевод организации",
-    "from": "Maestro 1596837868705199",
-    "to": "Счет 64686473678894779589"
+    "date": "2019-08-26T10:50:58.294041"
   },
-  {
-    "id": 41428829,
-    "state": "EXECUTED",
-    "date": "2019-07-03T18:35:29.512364",
-    "operationAmount": {
-      "amount": "8221.37",
-      "currency": {
-        "name": "USD",
-        "code": "USD"
-      }
-    },
-    "description": "Перевод организации",
-    "from": "MasterCard 7158300734726758",
-    "to": "Счет 35383033474447895560"
-  },
-  {
+    {
     "id": 939719570,
     "state": "EXECUTED",
     "date": "2018-06-30T02:08:58.425572",
-    "operationAmount": {
-      "amount": "9824.07",
-      "currency": {
-        "name": "USD",
-        "code": "USD"
-      }
-    },
-    "description": "Перевод организации",
-    "from": "Счет 75106830613657916952",
-    "to": "Счет 11776614605963066702"
-  }
-  ]) == [{
-    "id": 939719570,
-    "state": "EXECUTED",
-    "date": "2018-06-30T02:08:58.425572",
-    "operationAmount": {
-      "amount": "9824.07",
-      "currency": {
-        "name": "USD",
-        "code": "USD"
-      }
-    },
-    "description": "Перевод организации",
-    "from": "Счет 75106830613657916952",
-    "to": "Счет 11776614605963066702"
-  },
-  {
-    "id": 41428829,
-    "state": "EXECUTED",
-    "date": "2019-07-03T18:35:29.512364",
-    "operationAmount": {
-    "amount": "8221.37",
-    "currency": {
-    "name": "USD",
-    "code": "USD"
+      },
+    {
+      "id": 41428829,
+      "state": "EXECUTED",
+      "date": "2019-07-03T18:35:29.512364",
     }
-    },
-    "description": "Перевод организации",
-    "from": "MasterCard 7158300734726758",
-    "to": "Счет 35383033474447895560"
-    },
+  ]) == [
     {
     "id": 441945886,
     "state": "EXECUTED",
     "date": "2019-08-26T10:50:58.294041",
-    "operationAmount": {
-    "amount": "31957.58",
-    "currency": {
-    "name": "руб.",
-    "code": "RUB"
     },
-      "description": "Открытие вклада",
-      "to": "Счет 72082042523231456215"
+    {
+      "id": 41428829,
+      "state": "EXECUTED",
+      "date": "2019-07-03T18:35:29.512364",
     },
+    {
+      "id": 939719570,
+      "state": "EXECUTED",
+      "date": "2018-06-30T02:08:58.425572",
+    }
   ]
