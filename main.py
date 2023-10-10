ba18fdb40b8d14efbefd15de691b261e2897890a
petrova_juliana <juliana6k6@gmail.com>
2023-10-10 03:52:23 +0300

    func main

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
-from src.utils import get_all_operations, filter_operations, sort_operations_five
+from src.utils import (get_all_operations, filter_operations,
+                       sort_operations_five)
 from src.utils2 import encode_cards, format_date, encode_score
 filename_ = "operations.json"
+
 
 all_operations = get_all_operations(filename_)
 filtered_operations = filter_operations(all_operations)
 sorted_operations_five = sort_operations_five(filtered_operations)
-sorted_operations_five.reverse()
-print(encode_cards("1234567812345678"))
-print(encode_score("12345678912345678912"))
+for operation in sorted_operations_five:
+    date = format_date(operation["date"])
+    description_operation = operation["description"]
+    initial_count = operation["from"]
+    final_count = operation["to"]
+    counts = [initial_count, final_count]
+    counts_new = []
+    for count in counts:
+        count_info = count.split()
+        if count_info[0].lower() == "счёт":
+            score_form = encode_score(count_info[1])
+        else:
+            score_form = encode_cards(count_info[1])
+        score = count_info[0] + score_form
+        score_split = " ".join(score)
+        counts_new.append(score)
+print(f""
